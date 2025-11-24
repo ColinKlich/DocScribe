a6fb98825020c1fe35c73ae7592398d5eda1c31d
cklich <cmklich@crimson.ua.edu>
2025-11-24 17:03:02 -0600

    updated eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -4,20 +4,19 @@ import { defineConfig } from "eslint/config";
 import obsidianmd from "eslint-plugin-obsidianmd";
 
 export default defineConfig([
-  ...obsidianmd.configs.recommended,
   {
-    files: ["**/*.ts"],
+    ignores: ["node_modules/", "main.js", "eslint.config.js"],
+  },
+  {
+    files: ["src/**/*.ts"],
+    plugins: {
+      obsidianmd,
+    },
     languageOptions: {
       parser: tsparser,
       parserOptions: { project: "./tsconfig.json" },
     },
-
-    // You can add your own configuration to override or add rules
-    rules: {
-      // example: turn off a rule from the recommended set
-      "obsidianmd/sample-names": "off",
-      // example: add a rule not in the recommended set and set its severity
-      "obsidianmd/prefer-file-manager-trash": "error",
-    },
+    rules: {},
   },
 ]);
+
